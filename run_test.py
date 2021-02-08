81ea1d36a4f397d3f61cecb598e7418197dd47d5
Kristina Ceres <kc649@cornell.edu>
2021-02-08 13:12:54 -0500

    change version run_test.py

--- run_test.py
+++ run_test.py
@@ -1,18 +1,16 @@
 # contains unit test functions for each module
 
 def debug(cauldron):
-  import cv2
-  import numpy as np
-  from matplotlib import pyplot as plt
+  from PIL import Image
   #import test 
 	
   if cauldron == 23:
-		# open method used to open different extension image file 
-	  print("You did it! Great job finding all of the bugs!")
-	  img = cv2.imread("potion.jpg")
-	  plt.imshow(img)
-	  plt.axis('off')
-	  plt.show()
+
+	image = Image.open('potion.jpg')
+	image.show()
+	# open method used to open different extension image file 
+	 print("You did it! Great job finding all of the bugs!")
+	  
   
   else:
 	  print("This spell still has bugs. Try to find some more!")
